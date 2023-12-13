30a4b5a419424748fd21accc74c6b029126f2e32
Salmen <jlalisalmen@gmail.com>
2023-12-13 09:25:51 +0100

    added variable age

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
+    val age = 25
+    println(age)
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
